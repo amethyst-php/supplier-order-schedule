cabff3335c179639877cac4bc915213ca7bf7831
railken <railken@protonmail.ch>
2020-07-23 16:08:04 +0200

    add validation

--- src/Schemas/SupplierOrderScheduleSchema.php
+++ src/Schemas/SupplierOrderScheduleSchema.php
@@ -5,6 +5,7 @@ namespace Amethyst\Schemas;
 use Railken\Lem\Attributes;
 use Railken\Lem\Schema;
 use Amethyst\Managers\SupplierManager;
+use Railken\Lem\Contracts\EntityContract;
 
 class SupplierOrderScheduleSchema extends Schema
 {
@@ -19,14 +20,21 @@ class SupplierOrderScheduleSchema extends Schema
             Attributes\IdAttribute::make(),
             Attributes\LongTextAttribute::make('description'),
             Attributes\TextAttribute::make('request_cron')
+                ->setValidator(function (EntityContract $entity, $value) {
+                    return \Cron\CronExpression::isValidExpression($value);
+                })
                 ->setRequired(true),
             Attributes\TextAttribute::make('delivery_cron')
+                ->setValidator(function (EntityContract $entity, $value) {
+                    return \Cron\CronExpression::isValidExpression($value);
+                })
                 ->setRequired(true),
             Attributes\TextAttribute::make('shipment_day')
                 ->setRequired(true),
             Attributes\BelongsToAttribute::make('supplier_id')
                 ->setRelationName('supplier')
                 ->setRelationManager(SupplierManager::class),
+
             Attributes\CreatedAtAttribute::make(),
             Attributes\UpdatedAtAttribute::make(),
             Attributes\DeletedAtAttribute::make(),
